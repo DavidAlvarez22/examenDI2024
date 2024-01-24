36da8b82d740a294b5fba56ccdfe0a4207c5e836
David <daalvarez@birt.eus>
2024-01-24 18:11:12 +0100

    Inicio

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'DI_PlantillaExamenParcial_2324',
+  appName: 'examen',
   webDir: 'www',
   server: {
     androidScheme: 'https'
